2cc2d28331ded5f90eb882fe21229083a3f188da
PickHD <taufikjanuar35@gmail.com>
2023-05-03 20:06:38 +0700

    task (auth) : initialize auth services

---
+++ auth/internal/v1/service/health_check.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"context"
+
+	"github.com/PickHD/singkatin-revamp/auth/internal/v1/config"
+	"github.com/PickHD/singkatin-revamp/auth/internal/v1/repository"
+)
+
+type (
+	// HealthCheckService is an interface that has all the function to be implemented inside health check service
+	HealthCheckService interface {
+		Check() (bool, error)
+	}
+
+	// HealthCheckServiceImpl is an app health check struct that consists of all the dependencies needed for health check service
+	HealthCheckServiceImpl struct {
+		Context         context.Context
+		Config          *config.Configuration
+		HealthCheckRepo repository.HealthCheckRepository
+	}
+)
+
+// NewHealthCheckService return new instances health check service
+func NewHealthCheckService(ctx context.Context, config *config.Configuration, healthCheckRepo repository.HealthCheckRepository) *HealthCheckServiceImpl {
+	return &HealthCheckServiceImpl{
+		Context:         ctx,
+		Config:          config,
+		HealthCheckRepo: healthCheckRepo,
+	}
+}
+
+func (hs *HealthCheckServiceImpl) Check() (bool, error) {
+	return hs.HealthCheckRepo.Check()
+}
