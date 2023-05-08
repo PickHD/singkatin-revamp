d1d697ef67c4c90499c4d5a7988bbe83e6bb5669
PickHD <taufikjanuar35@gmail.com>
2023-05-08 18:58:28 +0700

    chore (SRS-06) : add timestamp to users collections

--- auth/internal/v1/model/user.go
+++ auth/internal/v1/model/user.go
@@ -1,13 +1,18 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type (
 	// User consist data of users
 	User struct {
-		ID       primitive.ObjectID `bson:"_id,omitempty"`
-		FullName string             `bson:"fullname,omitempty"`
-		Email    string             `bson:"email,omitempty"`
-		Password string             `bson:"password,omitempty"`
+		ID        primitive.ObjectID `bson:"_id,omitempty"`
+		FullName  string             `bson:"fullname,omitempty"`
+		Email     string             `bson:"email,omitempty"`
+		Password  string             `bson:"password,omitempty"`
+		CreatedAt time.Time          `bson:"created_at,omitempty"`
 	}
 )
